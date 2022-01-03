85c890cc56a6a14225807cb2760a9345104088c1
jsk900 <jsk900@yahoo.co.uk>
2022-01-03 12:08:20 +0100

    react-weather

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import { useContext } from 'react';
 import './App.css';
 
+import MyContext from './context/MyContext';
+import Weather from './components/Weather';
+import DropDowns from './components/DropDowns';
+
 const App = () => {
+  const context = useContext(MyContext);
+  const { citySelect } = context;
+
   return (
     <main>
-     <h1>Welcome to React</h1>
+      <DropDowns />
+      {citySelect && <Weather />}
     </main>
   );
-}
+};
 
 export default App;
-
